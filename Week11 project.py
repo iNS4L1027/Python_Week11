754119e2336d064e16876f086e6ee22a76f42b57
iNS4L1027 <insal102727@jwu.ac.kr>
2023-11-20 11:09:47 +0900

    파일 이름 변경

--- Week11 project.py
+++ Week11 project.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter.simpledialog import *
 from tkinter import messagebox
 import webbrowser
+from random import choice
 
 #함수 선언
 def place():
@@ -18,6 +19,23 @@ def open():
 def exit():
     window.quit()
     window.destroy()
+def play_game(player_choice):
+    choices = ['가위', '바위', '보']
+    computer_choice = choice(choices)
+
+    if player_choice == computer_choice:
+        result = "비겼습니다!"
+    elif (
+        (player_choice == '가위' and computer_choice == '보') or
+        (player_choice == '바위' and computer_choice == '가위') or
+        (player_choice == '보' and computer_choice == '바위')
+    ):
+        result = "이겼습니다!"
+    else:
+        result = "졌습니다!"
+
+    messagebox.showinfo("가위바위보 게임 결과", f"플레이어: {player_choice}\n컴퓨터: {computer_choice}\n결과: {result}")
+
 
 #창 설정
 window = Tk()
@@ -43,6 +61,18 @@ button2 = Button(window, text="\'Place()\'의 기능 설명", bg="#424242", font
 button3 = Button(window, text="\'Dialog\'의 기능 설명", bg="#424242", font="맑은고딕", fg="White",command=dialog)
 button4 = Button(window, text="\'Dialog\'의 기능 예제", bg="#424242", font="맑은고딕", fg="White",command=dialogTest)
 
+
+#이미지
+Rock = PhotoImage(file="RSP/Rock.png")
+Scissors = PhotoImage(file="RSP/Scissors.png")
+Paper = PhotoImage(file="RSP/Paper.png")
+
+#가위바위보
+rock_button = Button(window, image=Rock, bg="#424242", command=lambda: play_game('바위'))
+paper_button = Button(window, image=Paper, bg="#424242", command=lambda: play_game('보'))
+scissors_button = Button(window, image=Scissors, bg="#424242", command=lambda: play_game('가위'))
+rsp = Label(window, text="가위바위보 게임", bg="#1C1C1C", font="맑은고딕", fg="White",)
+
 #Dialog 예제
 Dialog1 = Label(window, text = "메모장", bg="#F7F8E0",)
 
@@ -52,5 +82,11 @@ button2.pack(side= TOP, fill=X, ipadx=10, ipady=10, padx=10,pady=12)
 button3.pack(side= TOP, fill=X, ipadx=10, ipady=10, padx=10,pady=12)
 button4.pack(side= TOP, fill=X, ipadx=10, ipady=10, padx=10,pady=12)
 Dialog1.pack(side= TOP, fill=X, ipadx=10, ipady=10, padx=10,pady=12)
+
+#가위바위보 앵커
+rsp.pack(side= LEFT, fill=X, ipadx=10, ipady=10, padx=10,pady=12)
+scissors_button.pack(side=LEFT, fill=X, ipadx=10, ipady=10, padx=10, pady=12)
+rock_button.pack(side=LEFT, fill=X, ipadx=10, ipady=10, padx=10, pady=12)
+paper_button.pack(side=LEFT, fill=X, ipadx=10, ipady=10, padx=10, pady=12)
 
 window.mainloop()
